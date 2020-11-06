aff0fabdff6f74d71c90089bcfea18ce15558ee1
Xavier Clerc <xclerc@janestreet.com>
2020-11-06 12:00:13 +0000

    v0.15~preview.124.03+65

--- src/timezone.ml
+++ src/timezone.ml
@@ -68,8 +68,8 @@ module Zone_cache = struct
 
   let to_alist () = Hashtbl.to_alist the_one_and_only.table
 
-  let initialized_zones t =
-    List.sort ~compare:(fun a b -> String.ascending (fst a) (fst b)) (to_alist t)
+  let initialized_zones () =
+    List.sort ~compare:(fun a b -> String.ascending (fst a) (fst b)) (to_alist ())
   ;;
 
   let find_or_load_matching t1 =
